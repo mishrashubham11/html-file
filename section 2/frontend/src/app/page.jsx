1f94ecf8a9775f7c31393daab957a445679cb8b7
mishrashubham11 <mishrashubham8756@gmail.com>
2024-05-31 20:09:36 +0530

    basic jsx

---
+++ section 2/frontend/src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <h1 style={{textAlign: 'center', fontSize:56, color:' green'}}>Home page </h1>
+     </div>
+
+  )
+}
+
+export default Home;
