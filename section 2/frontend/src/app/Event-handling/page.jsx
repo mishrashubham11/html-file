e161c1127a032e76b352d9e0361da749dda3f299
mishrashubham11 <mishrashubham8756@gmail.com>
2024-06-04 20:10:41 +0530

    EVENT HANDLING

--- section 2/frontend/src/app/Event-handling/page.jsx
+++ section 2/frontend/src/app/Event-handling/page.jsx
@@ -1,11 +1,26 @@
+'use client'; // for on click working
 import React from 'react'
 
 const EventHandling = () => {
   return (
-    <div>
+    <div onMouseMove={(e)=>{console.log(e.clientX, e.clientY);}} className='px-64'> 
       <h1 className='text-center my-4 text-4xl font-bold'>Event Handling</h1>
       
+<button onClick={()=>{alert ('click event performed')}}
+ className='p-4 bg-violet-600 text-white rounded-md '
+ > Click me</button>
+{/* upper on is for events with use of arrow function */}
 
+
+
+
+<input onChange={(event)=>{console.log(event.target.value)}} className='block mt-4 border-2 border-violet-600 p-3 w-full' type="text" />
+<input onChange={(event)=>{document.body.style.background=event.target.value}} type="color" />
+{/* the dom upper for change body clr with input typ color */}
+
+
+<input onChange={(e)=>{console.log(e.target.file);}} type="file" />
+{/* the upper input file give option of file in browser */}
 
 
 
